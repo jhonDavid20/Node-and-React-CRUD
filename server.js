29e0005fc0c242ba54707fffccff708c2b44327b
Jhonathan David <jhonps396@gmail.com>
2021-01-07 12:45:49 -0600

    Adding main files

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
