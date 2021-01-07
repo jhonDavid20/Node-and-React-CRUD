b2161f8d3f023d662040627644c2e7c35446a71c
Jhonathan David <jhonps396@gmail.com>
2021-01-07 12:58:44 -0600

    setting up server

--- server.js
+++ server.js
@@ -10,3 +10,22 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+
+// parse request of content-type - application/json
+app.use(bodyParser.json());
+
+// parse request of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true}));
+
+// simple route
+app.get("/", (req,res) => {
+    res.json({ message: "Welcome to CRUD application"});
+});
+
+//set port, listen for requests
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
+
+
